0367c4bd244c6e7be98a64c44f4a04244ca15675
thefirmt <thefirmt@gmail.com>
2019-07-15 16:29:20 -0700

    stringify works

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null || Number.isNaN(obj) || obj === Infinity) {
+    return "null";
+  }
+  if (typeof obj === "boolean" || typeof obj === "number") {
+    return "" + obj;
+  }
+  if (typeof obj === "string") {
+    return ('"' + obj + '"');
+  }
+  if (obj === undefined || typeof obj === "function") {
+    return undefined;
+  }
+  if (Array.isArray(obj)) {
+    var output = "[";
+    for (var i = 0; i < obj.length; i++) {
+      if (obj[i] === undefined || typeof obj[i] === "function" || Number.isNaN(obj[i]) || obj[i] === Infinity) {
+        output += "null";
+      } else {
+        output += stringifyJSON(obj[i]);
+      }
+      output += ",";
+    }
+    output += "]";
+    var newOutput = output.replace(",]", "]");
+    return newOutput;
+  } else {
+    var output = "{";
+    for (var key in obj) {
+      if (obj[key] === "undefined" || typeof obj[key] === "function") {
+        continue;
+      } 
+      if (key === "undefined") {
+        continue;
+      } else if (key) {
+        output += '"' + key + '":';
+      }
+      output += stringifyJSON(obj[key]);
+      output += ",";
+    }
+    output += "}";
+    var newOutput = output.replace(",}", "}");
+    return newOutput;
+  } 
 };
+
